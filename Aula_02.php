a12d8ea40a8e2d803ce148cb7a8075fa71580604
Cristian Camargo <cristian.camargo@outlook.com.br>
2018-01-29 22:40:03 -0200

    Meu Primeiro Commit no GIT

---
+++ Aula_02.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta charset = "utf-8" />
+        <meta name = "viewport" content = "width=device-width, initial-scale = 1.0" />
+        <meta name = "X-UA-Compatible" content = "ie-edge" />
+        <title>Git Inicio</title>
+    </head>
+    <body>
+        <?= "Hello World"; ?>
+    
+    </body>
+</html>
+
+
+<!-- SCM = SOURCE CODE MANAGEMENT -->
